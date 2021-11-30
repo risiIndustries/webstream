3efcc06e7981eecdecc099eeec2017326249eca9
PizzaLovingNerd <pizzalovingnerd@teknik.io>
2021-11-30 12:35:09 -0800

    More webstream updates
    - Merged tags
    - Categories are now a list instead of a string

--- __main__.py
+++ __main__.py
@@ -42,7 +42,14 @@ class Storage:
     def get_apps_by_category(self, category):
         apps = []
         for app in self.apps:
-            if app.category == category:
+            if category in app.categories:
+                apps.append(app)
+        return apps
+    
+    def get_apps_by_tag(self, tag):
+        apps = []
+        for app in self.apps:
+            if tag in app.tags:
                 apps.append(app)
         return apps
 
@@ -83,11 +90,11 @@ class Storage:
         return False
 
 class WebApp:
-    def __init__(self, appid, name, url, homepage, description, category, tags):
+    def __init__(self, appid, name, url, homepage, description, categories, tags):
         self.appid = appid
         self.name = name
         self.url = url
         self.homepage = homepage
         self.description = description
-        self.category = category
+        self.categories = categories
         self.tags = tags
